7133380d71a584e8a2d04c7006cec32fc056d685
mohangrg2018 <mohangrg2030@gmail.com>
2025-08-17 13:38:34 +0545

    feat(footer): created footer with contact information

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,7 +1,60 @@
+import { assets } from "@/assets/assets";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-20 sm:mt-32">
+      <div className="container__width flex flex-col sm:flex-row gap-10">
+        <div className="sm:w-[60%] flex flex-col gap-2 items-center sm:items-start">
+          <Link to="/">
+            <img src={assets.logo} className="w-24 sm:w-32" alt="" />
+          </Link>
+          <p className="w-full sm:w-[80%] text-sm text-gray-400 text-center sm:text-start">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius quas
+            repudiandae, aliquid excepturi provident corporis, mollitia maiores
+            soluta aperiam eos harum libero asperiores distinctio voluptates
+            esse ratione maxime? Beatae, minima?
+          </p>
+        </div>
+        <div className="sm:w-[20%] flex flex-col gap-2">
+          <p className="font-semibold uppercase hidden sm:block">company</p>
+          <ul className="flex sm:flex-col gap-6 sm:gap-2 justify-center text-gray-400">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/collection">Collection</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="sm:w-[20%] flex flex-col gap-2">
+          <p className="font-semibold uppercase hidden sm:block">
+            get in touch
+          </p>
+          <ul className="flex sm:flex-col gap-6 sm:gap-2 justify-center text-gray-400">
+            <li>
+              <a href="tel:+123456789">+123456789</a>
+            </li>
+            <li>
+              <a href="mailto:uMl0D@example.com">uMl0D@example.com</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="container__width py-4 border-t border-gray-200 mt-6">
+        <p className="text-center text-gray-400 text-sm">
+          &copy; {new Date().getFullYear()} All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/components/shared/Subscription.jsx
+++ client/src/components/shared/Subscription.jsx
@@ -3,6 +3,11 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 const Subscription = () => {
+  const [email, setEmail] = React.useState("");
+  const handleSubscribe = (e) => {
+    e.preventDefault();
+    setEmail("");
+  };
   return (
     <section className="flex flex-col gap-4">
       <p className="capitalize font-bold text-2xl text-center">
@@ -11,9 +16,16 @@ const Subscription = () => {
       <p className="text-center text-gray-400">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi dolor
       </p>
-      <form className="flex items-center w-[90%] sm:w-[80%] lg:w-[50%] mx-auto">
-        <Input placeholder="Enter your email" />
-        <Button>Subscribe</Button>
+      <form
+        onSubmit={handleSubscribe}
+        className="flex items-center w-[90%] sm:w-[80%] lg:w-[50%] mx-auto"
+      >
+        <Input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          value={email}
+        />
+        <Button type="submit">Subscribe</Button>
       </form>
     </section>
   );
